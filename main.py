cea9f5081e8a67a63c1ab47f77d39cf1c5ee8ae6
praveenc1902 <mailpraveenreddy.c@gmail.com>
2025-08-12 19:37:25 +0530

    v3

--- main.py
+++ main.py
@@ -474,6 +474,7 @@ async def chat_with_agent(msg: MessageInput):
     return {"reply": reply}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
